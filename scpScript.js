13bac2f5388e464238cda139870650a75906ac5e
Graer Sultan Styne <graer.styne46@stu.bryanisd.org>
2025-02-04 20:34:09 -0600

    Create scpScript.js

---
+++ scpScript.js
@@ -0,0 +1,9 @@
+document.getElementById("menu1").addEventListener("click", menuExtender);
+function menuExtender()
+{
+    var menu = document.getElementById("menu1");
+    var list = document.getElementById("list1");
+    
+    menu.classList.toggle("extended");
+    list.classList.toggle("visible");
+}
